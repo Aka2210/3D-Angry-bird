9aaaa67137893635849d5beb2459af98089978e6
Aka2210 <qoo0973215128@gmail.com>
2023-12-07 20:57:11 +0800

    Finish bomb bird

--- FinalProject/Assets/Scripts/TNTCollider.cs
+++ FinalProject/Assets/Scripts/TNTCollider.cs
@@ -2,12 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TNTCollider : MonoBehaviour
+//繼承至BombCollider(從角色那邊繼承爆破函式來應用)
+public class TNTCollider : BombCollider
 {
-    [SerializeField] float triggerForce;
-    [SerializeField] float explosionRadius;
-    [SerializeField] float explosionForce;
-    [SerializeField] GameObject particles;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,30 +17,12 @@ public class TNTCollider : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision)
     {
         //如果碰撞的力>=觸發TNT所需的力則進行TNT爆炸程式碼
-        if(collision.relativeVelocity.magnitude >= triggerForce)
+        if (collision.relativeVelocity.magnitude >= triggerForce)
         {
-            //利用Physics抓取以transform.position為中心, explosionRadius為半徑的橢圓範圍內所有的物件
-            var surroundingObject = Physics.OverlapSphere(transform.position, explosionRadius);
-            
-            foreach(var obj in surroundingObject)
-            {
-                var rb = obj.GetComponent<Rigidbody>();
-                if (rb == null) continue;
-
-                //給予所有附近物件一爆破力(模擬爆炸的力)，參數分別為(爆炸力量, 爆炸中心點, 爆炸半徑)
-                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
-            }
-
-            //創建此物件爆炸粒子效果
-            GameObject explosive = Instantiate(particles, transform.position, Quaternion.identity);
-
-            //刪除TNT物件(使用gameObject連同父、子物件一起刪除)
-            Destroy(gameObject);
-            //兩秒後刪除爆炸用的粒子效果，避免堆疊太多導致卡頓
-            Destroy(explosive, 2);
+            Explosion(collision);
         }
     }
 }
