259da103169aadbc1970e9fa6ab234f0593eb890
YeyeLeaf <ling930718@gmail.com>
2023-12-23 12:16:48 +0800

    Fix Chuck and Add Terence

--- FinalProject/Assets/Scripts/ChuckCollider.cs
+++ FinalProject/Assets/Scripts/ChuckCollider.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChuckCollider : MonoBehaviour
+public class ChuckCollider : BirdCommonVar
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] GameObject ThrowingObject;
     [SerializeField] Transform ThrowingOrient;
-    bool isEnhance = false;
-    Vector3 currentRotation;
+    bool isEnhance = false;　//是否觸發技能
+    Vector3 currentRotation; // 目前rotation
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +18,7 @@ public class ChuckCollider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //觸發技能:先停止前進進行旋轉再施加力量
         if (Input.GetKeyDown(KeyCode.R) && isEnhance == false)
         {
             isEnhance = true;
@@ -26,19 +27,34 @@ public class ChuckCollider : MonoBehaviour
             rb.angularVelocity = Vector3.zero;
             StartCoroutine(EnhanceAndThrow());
         }
+        //掉下去就掰掰
+        if (GetComponent<Transform>().position.y <= -2)
+        {
+            Destroy(gameObject, 1f);
+        }
     }
     IEnumerator EnhanceAndThrow()
     {
-        while (transform.eulerAngles.x < 30)
+        //將Chuck的旋轉角度轉到30度
+        if(currentRotation.x > 300) currentRotation.x -= 360;
+        Debug.Log("Current X Rotation: " + currentRotation.x);
+        while (transform.eulerAngles.x > 80 || transform.eulerAngles.x < 30)
         {
-            float newRotationX = currentRotation.x + 30f * Time.deltaTime;
+            float newRotationX = currentRotation.x + 100f * Time.deltaTime;
             transform.rotation = Quaternion.Euler(newRotationX, currentRotation.y, currentRotation.z);
+            currentRotation = transform.eulerAngles;
             yield return null;
         }
-
-        int throwPower = 50;
+        //對Chuck施加較強的力讓他俯衝
+        int throwPower = 80;
         rb.AddForce(ThrowingOrient.forward * throwPower, ForceMode.Impulse);
 
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //撞到東西後三秒消失
+        HasCollider = true;
+        Destroy(gameObject, 3.0f);
     }
 }
