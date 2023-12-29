90a17a1efc221c794da6cf28e08c985fe29bcf72
Aka2210 <qoo0973215128@gmail.com>
2023-12-30 01:36:45 +0800

    The blues setting

--- FinalProject/Assets/Scripts/TheBluesCollider.cs
+++ FinalProject/Assets/Scripts/TheBluesCollider.cs
@@ -10,30 +10,31 @@ public class TheBluesCollider : BirdCommonVar
     void Start()
     {
         //把不要的丟到天邊
-        helmet.position = new Vector3(0, -1000, 0);
+        /*helmet.position = new Vector3(0, -1000, 0);
         bird2.position = new Vector3(0, -1000, 0);
-        bird3.position = new Vector3(0, -1000, 0);
+        bird3.position = new Vector3(0, -1000, 0);*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        base.Update();
         //按下R世界就會分裂成三半
-        if (Input.GetKeyDown(KeyCode.R))
+        /*if (Input.GetKeyDown(KeyCode.R))
         {
             if(isSplit == false)
             {
                 isSplit = true;
                 Split();
             }
-        }
+        }*/
     }
-    void Split()
+    /*void Split()
     {
         //把不小心遺棄的孩子撿回來
         bird2.position = bird1.position;
         bird3.position = bird1.position;
-    }
+    }*/
     private void OnCollisionEnter(Collision collision)
     {
         HasCollider = true;
