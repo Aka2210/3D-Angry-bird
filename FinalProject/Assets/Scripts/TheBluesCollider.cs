b0a00020854e32c5adfa37761f454400675b5e84
YeyeLeaf <ling930718@gmail.com>
2024-01-01 13:10:03 +0800

    fix theblues bug

--- FinalProject/Assets/Scripts/TheBluesCollider.cs
+++ FinalProject/Assets/Scripts/TheBluesCollider.cs
@@ -7,10 +7,12 @@ public class TheBluesCollider : BirdCommonVar
     // Start is called before the first frame update
     [SerializeField] Transform helmet, bird1, bird2, bird3;
     [SerializeField] Rigidbody rb1, rb2, rb3, rbH;
+    [SerializeField] Collider c, c1, c2, c3, cH;
     [SerializeField] int throwPower;
     bool isSplit = false;
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -30,6 +32,11 @@ public class TheBluesCollider : BirdCommonVar
     void Split()
     {
         //分別丟出去 安全帽下去
+        c1.enabled = true;
+        c2.enabled = true;
+        c3.enabled = true;
+        cH.enabled = true;
+        c.enabled = false;
         rb1.isKinematic = false;
         rb2.isKinematic = false;
         rb3.isKinematic = false;
