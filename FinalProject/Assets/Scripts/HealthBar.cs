b936aa327aed87d9c04ed344fe498b87099bb521
YeyeLeaf <ling930718@gmail.com>
2024-01-06 02:37:06 +0800

    add guideline

--- FinalProject/Assets/Scripts/HealthBar.cs
+++ FinalProject/Assets/Scripts/HealthBar.cs
@@ -7,16 +7,17 @@ public class HealthBar : MonoBehaviour
     float deltaHight;
     private void Start()
     {
-        deltaHight = transform.position.y - pig.transform.position.y + 1f;
+        deltaHight = transform.position.y - pig.transform.position.y + 1f; //calculate the distance between the healthbar and the pig 
     }
     private void Update()
     {
-        transform.position = new Vector3(pig.transform.position.x, pig.transform.position.y + deltaHight, pig.transform.position.z);
+        transform.position = new Vector3(pig.transform.position.x, pig.transform.position.y + deltaHight, pig.transform.position.z); //fix relative y position
     }
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
         if (_healthbarSprite != null)
         {
+            //calculate the ratio of blood in healthbar
             currentHealth = currentHealth >= 0 ? currentHealth : 0;
             _healthbarSprite.fillAmount = currentHealth / maxHealth;
         }
--- FinalProject/Assets/Scripts/MatildaCollider.cs
+++ FinalProject/Assets/Scripts/MatildaCollider.cs
@@ -19,7 +19,7 @@ public class MatildaCollider : BirdCommonVar
     {
         base.Update();
 
-        //�p�GR�Q���U�ӥB�|���ϥιL�p���ޯ�h����p���ޯ�üаO���w�ϥιL�ޯ�
+        //press R to lay egg
         if (Input.GetKeyDown(KeyCode.R) && isLayEgg == false)
         {
             LayEgg();
@@ -29,15 +29,15 @@ public class MatildaCollider : BirdCommonVar
 
     private void LayEgg()
     {
-        //�]�w�z�����J�ͦ���m�A+0.5f��offset�קK�@�ͦ��N����p�����I���c�ɭP�z��
+        //下蛋
         Vector3 temp = this.transform.position + Vector3.down * 2f;
         GameObject clonedObject = Instantiate(eggBomb, temp, Quaternion.identity);
 
-        //�]�w�p���U�J��V�V�W�B�e�����O�q
+        //讓鳥稍微往上飛
         int ThrowPowerX = 30, ThrowPowerY = 50;
         rb.AddForce(ThrowingOrient.forward * ThrowPowerX + ThrowingOrient.up * ThrowPowerY, ForceMode.Impulse);
 
-        //�Q���R�������J����A�T�O���J�Y�ϱ��i��Ť]�|�Q�R��
+        //最晚10秒後egg消失
         Destroy(clonedObject, 10.0f);
     }
 
@@ -45,7 +45,7 @@ public class MatildaCollider : BirdCommonVar
     {
         if (collision.collider.tag != "Player")
         {
-            //��X��Y�I���쪫��h�����U�J�\��æb�T����R���p��
+            //3秒後鳥消失
             HasCollider = true;
             isLayEgg = true;
             Destroy(gameObject, 3.0f);
